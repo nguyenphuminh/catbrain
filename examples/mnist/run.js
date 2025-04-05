4ff03b9e5a2e2f0ace4c7c3e6178e6bc7bd215b7
Phu Minh (Catto) <53084840+nguyenphuminh@users.noreply.github.com>
2025-04-05 13:24:31 +0700

    Nicer examples :)

--- examples/mnist/run.js
+++ examples/mnist/run.js
@@ -2,21 +2,25 @@ const mnist = require("mnist");
 const { CatBrain } = require("../../dist/core");
 const fs = require("fs");
 
-// Generate the dataset
-const set = mnist.set(0, 1);
-// Normalized sample
-const sample = set.test[0].input.map(el => Math.round(el));
 // Load the model
 const neuralNetwork = new CatBrain(JSON.parse(fs.readFileSync("./model.json")));
-// Log out the image
-logImage(sample);
-// Test it out :)
-console.log(`\nNumber: ${indexOfMaxValue(neuralNetwork.feedForward(sample))}`);
+
+setInterval(() => {
+    // Generate the dataset
+    const set = mnist.set(0, 1);
+    // Normalized sample
+    const sample = set.test[0].input.map(el => Math.round(el));
+    // Log out the image
+    logImage(sample);
+    // Test it out :)
+    console.log(`\nNumber: ${indexOfMaxValue(neuralNetwork.feedForward(sample))}`);
+    console.log("Next demo in 3 seconds...\n");
+}, 3000);
 
 // Function to log out the image
 function logImage(dataObject) {
     for (let i = 0; i < dataObject.length; i++) {
-        process.stdout.write((dataObject[i] ? "0" : "1") + " ");
+        process.stdout.write((dataObject[i] ? "0" : "1"));
 
         if ((i + 1) % 28 === 0) {
             console.log("");
